1cb6bc2cc98a4425a90b877aa0058c8e04c78b87
DJ Daniels <dj.daniels@ampush.com>
2015-08-20 20:37:33 -0700

    dwi

--- bin/main.py
+++ bin/main.py
@@ -7,10 +7,11 @@ from threading import *
 
 
 DEF_PORT_LIST = [
-    1,5,7,18,20,21,22,23,25,29,37,42,43,49,53,69,70,79,80,103,108,109,110,115,118,119,137,139,143,150,156,161,9090
+    1,5,7,18,20,21,22,23,25,29,37,42,43,49,53,69,70,79,80,103,108,109,110,115,118,119,137,139,143,150,156,161,9090,
     179,190,194,197,389,396,443,444,445,458,546,547,563,569,1080,201,4380,4370,17603,17600,49588,15292,6263,6258,8080,
-    8081,9091
-]
+    8081,9091,9000, 9999, 9998, 9997, 138, 873, 3260, 6281, 5000, 5001, 995, 3005, 5353, 8324, 32469
+] + list(range(6890, 6999))
+
 
 
 screenLock = Semaphore(value=1)
@@ -19,7 +20,7 @@ def connScan(tgtHost, tgtPort, quiet):
         connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
         connSkt.send('jsdfijidsjfoi\r\n')
-        results = connSkt.recv(100)
+        results = connSkt.recv(1024)
         screenLock.acquire()
         print '     [+]%d/tcp open' % tgtPort
         if (results == None) | (results == ''):
@@ -40,7 +41,7 @@ def portScan(tgtHost, tgtPorts, quiet):
     try:
         tgtIP = gethostbyname(tgtHost)
     except:
-        print "[-] Cannot resolve '%s' Unknown host" %tgtHost
+        print "[-] Cannot resolve '%s' Unknown host" % tgtHost
         return
     try:
         tgtName = gethostbyaddr(tgtIP)
@@ -50,16 +51,16 @@ def portScan(tgtHost, tgtPorts, quiet):
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
         t = Thread(target=connScan, args=(tgtHost, int(tgtPort), quiet), name='scan[%s:%s]' % (tgtHost, tgtPort))
-        t.start();
-        time.sleep (0.10);
-
+        t.start()
+        time.sleep(0.1)
 
 
 def main():
     parser = optparse.OptionParser('usage %prog -H' + '<target host> -p <target port>')
     parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-    parser.add_option('-q', action='store_true', dest='quiet', help='specify quiet mode, will not show closed ports')
-    parser.add_option('-p', dest='tgtPort', type='string', help='specify target ports', default=','.join("{0}".format(n) for n in DEF_PORT_LIST))
+    parser.add_option('-q', dest='quiet', action='store_true', help='specify quiet mode, will not show closed ports')
+    parser.add_option('-p', dest='tgtPort', type='string', help='specify target ports', \
+        default=','.join("{0}".format(n) for n in DEF_PORT_LIST))
     (options, args) = parser.parse_args()
     tgtHost = options.tgtHost
     tgtPorts = str(options.tgtPort).split(',')
